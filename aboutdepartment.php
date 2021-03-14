f249666c7bf4d9981bc5bbf201cda24b406fc353
Mathew-Varghese28 <mca526@rajagiri.edu>
2021-03-14 17:17:05 +0530

    website

--- aboutdepartment.php
+++ aboutdepartment.php
@@ -60,7 +60,7 @@
             </div>
             <div class="col col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8 col-xxl-8">
                 <h1><b><u><center>DEPT. OF COMPUTER SCIENCE</center></u></b></h1>
-                <img height="50px" src="C:\xampp\htdocs\college\images\group-of-students-looking-at-computer-monitor.jpg" class="img-thumbnail" alt="...">
+                <img height="50px" src="images\group-of-students-looking-at-computer-monitor.jpg" class="img-thumbnail" alt="...">
                 <p>
                     The Department commenced its two year (four semesters) Master of Computer Applications (MCA) Programme in 2001. Affiliated to Mahatma Gandhi University, Kottayam, Kerala, India, and approved by the All India Council for Technical Education, New Delhi, the course is designed to meet the ever-growing demand for well qualified and trained computer programmers. The Department has been consecutively rated “Platinum” by the AICTE-CII survey, for its illustrious placement track record and distinctive industry interaction programmes. The various specialisations of the MCA programme at RCSS brings out the students as world class professionals, suitable for the software industry.
             
--- activitiesdept.php
+++ activitiesdept.php
@@ -61,7 +61,7 @@
             <div class="col col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8 col-xxl-8">
                 <h1><b><u><center>DEPT. OF COMPUTER SCIENCE</center></u></b></h1>
                 <h2><u>ACTIVITIES & EVENTS</u></h2>
-                <img height="50px" src="C:\xampp\htdocs\college\images\cover-image-3-1024x537.jpg" class="img-thumbnail" alt="...">
+                <img height="50px" src="images\cover-image-3-1024x537.jpg" class="img-thumbnail" alt="...">
                 <p>
                     Euphoria is the biennial national Student Fest organized by Dept. of Computer Science and Computer Students Association of Rajagiri (C.S.T.A.R). This technical cum cultural extravaganza is one of the most anticipated college fest in India with an expected participation of 300+ Colleges with 700+ students, Euphoria 2k19 will be one of the epic college fest conducted in South India. The fest comes with the most attractive cash prizes, placement opportunities and Innovative gateways. For more details-http://euphoria2k19.com/
             
